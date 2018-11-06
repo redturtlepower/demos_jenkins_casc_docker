a88c47896e326ea5b1672da05094153a2ef36c87
thomas <thomas.rusche@gmail.com>
2018-11-07 00:00:05 +0100

    added script that generates ssh key pairs and deploys the public key to each slave

---
+++ v6/create-and-deploy-ssh-keys.sh
@@ -0,0 +1,49 @@
+# Parameters
+REDEPLOY_EXISTING_KEY=false
+
+# Helper variables
+KEY_IS_NEW=false
+
+if [ ! -f ~/.ssh/jenkins_slave.pub ]; then
+    echo -n "SSH key 'jenkins_slave' has not been found in ~/.ssh/! Creating new key pair and deploying to slaves..."
+    ssh-keygen -t rsa -C "jenkins build slave key" -f ~/.ssh/jenkins_slave
+    KEY_IS_NEW=true
+else
+    echo -n "The key 'jenkins_slave' already exists. Do you still want to (re-)deploy it to the hosts? Enter (y) or (n) and press [ENTER]: "
+    read -n 2 deploy_decision
+    if [ "$deploy_decision" == "y" ]; then
+        REDEPLOY_EXISTING_KEY=true
+    elif [ "$deploy_decision" == "n" ]; then
+        REDEPLOY_EXISTING_KEY=false
+    else
+        printf "\nWrong input. Aborting...\n"
+        exit 1
+    fi
+fi
+
+echo -n "KEY_IS_NEW:" $KEY_IS_NEW 
+printf "\n"
+echo -n "REDEPLOY_EXISTING_KEY:" $REDEPLOY_EXISTING_KEY 
+printf "\n"
+
+if [ "$KEY_IS_NEW" == true ] || [ "$REDEPLOY_EXISTING_KEY" == true ]; then
+    printf "Deploying key to slaves...\n"
+
+    printf "Copy to Mac ----------------------------------------------------------\n"
+    # Remove host from known_hosts just in case the key has changed.
+    ssh-keygen -f "~/.ssh/known_hosts" -R "[192.168.2.221]:22"
+    # Copy to mac slave
+    ssh-copy-id -i ~/.ssh/jenkins_slave.pub jenkins@192.168.2.220
+
+    printf "Copy to Ubuntu ----------------------------------------------------------\n"
+    # Remove host from known_hosts just in case the key has changed.
+    ssh-keygen -f "~/.ssh/known_hosts" -R "[192.168.2.221]:2030"
+    # Copy to ubuntu slave - be sure that 'vagrant up ubuntu' has been run successfully on mac!
+    ssh-copy-id -i ~/.ssh/jenkins_slave.pub -p 2030 jenkins@192.168.2.221
+
+    printf "Copy to Wine ----------------------------------------------------------\n"
+    # Remove host from known_hosts just in case the key has changed.
+    ssh-keygen -f "~/.ssh/known_hosts" -R "[192.168.2.221]:2040"
+    # Copy to wine slave (windows on ubuntu) - be sure that 'vagrant up wine' has been run successfully on mac!
+    ssh-copy-id -i ~/.ssh/jenkins_slave.pub -p 2040 jenkins@192.168.2.221
+fi
